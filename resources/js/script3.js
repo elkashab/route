b5fbec0657053e2a89f0781d39609300e091e7d0
elkashab <elkashab@gmail.com>
2025-06-27 17:11:03 +0300

    Third Commit

--- resources/js/script3.js
+++ resources/js/script3.js
@@ -1,3 +1,8 @@
+let logoutBtn = document.querySelector("#logout");
 let welcomeElement = document.querySelector("h3");
 let userSession = localStorage.getItem("sessionUsername");
 welcomeElement.innerText = `Welcome ${userSession}`;
+
+logoutBtn.addEventListener("click", function () {
+  localStorage.removeItem("sessionUsername");
+});
